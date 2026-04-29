cdbe8b107207ff2e2c24a450a17b621f4943b9cc
Samuel Kratoš <fake@mail.fake>
2026-04-29 11:12:43 +0200

    WalletsRepository

--- src/KryptoSmenarna.Wpf/Data/WalletsRepository.cs
+++ src/KryptoSmenarna.Wpf/Data/WalletsRepository.cs
@@ -6,10 +6,53 @@ using System.Threading.Tasks;
 using Oracle.ManagedDataAccess.Client;
 using System.Data;
 using System.Windows.Media.Animation;
+using KryptoSmenarna.Wpf.Models;
 namespace KryptoSmenarna.Wpf.Data
 {
     public class WalletsRepository
     {
+        public List<Wallet> GetAllWallets(int userId, bool isCrypto)
+        {
+            List<Wallet> result = new List<Wallet>();
+
+            using OracleConnection connection = new OracleConnectionFactory().CreateConnection();
+            connection.Open();
+
+            string sql = @"
+        SELECT
+            w.wallet_id,
+            w.user_id,
+            w.currency_code,
+            w.balance
+        FROM wallets w
+        JOIN currencies c ON c.currency_code = w.currency_code
+        WHERE w.user_id = :p_user_id
+          AND c.is_crypto = :p_is_crypto
+        ORDER BY w.currency_code";
+
+            using OracleCommand command = new OracleCommand(sql, connection);
+            command.BindByName = true;
+
+            command.Parameters.Add("p_user_id", OracleDbType.Int32).Value = userId;
+            command.Parameters.Add("p_is_crypto", OracleDbType.Int32).Value = isCrypto ? 1 : 0;
+
+            using OracleDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Wallet wallet = new Wallet()
+                {
+                    wallet_id = Convert.ToInt32(reader["wallet_id"]),
+                    user_id = Convert.ToInt32(reader["user_id"]),
+                    currencyCode = reader["currency_code"].ToString(),
+                    balance = Convert.ToDecimal(reader["balance"])
+                };
+
+                result.Add(wallet);
+            }
+
+            return result;
+        }
         public bool TryWithdraw(decimal amount, string currencyCode, int userId) //pokusi se vybrat částku z dené peněženky, pokud uspěje, vrátí true
         {
             try
---
+++ src/KryptoSmenarna.Wpf/Models/Wallet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KryptoSmenarna.Wpf.Models
+{
+    public class Wallet
+    {
+        public int wallet_id { get; set; }
+        public int user_id { get; set; }
+        public string currencyCode {  get; set; }
+        public decimal balance { get; set; }
+    }
+}
